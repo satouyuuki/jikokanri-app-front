629d9e101994f93a16cf94e232bd1f9e28599e3f
satoyuki <yuki01377@gmail.com>
2021-03-21 02:26:02 +0900

    ec2 request custom domain name

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT!;
 export const api = axios.create({
-  baseURL: API_ENDPOINT,
+  baseURL: API_ENDPOINT + 'api/',
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json'
 });
